ee34819fa98a454da2e2b4e337a56e8ba555f5fe
JeanetteMoKa <zsanettkasek@gmail.com>
2024-08-26 14:40:20 +0200

    favicon

--- SolarWatch/Frontend/vite.config.js
+++ SolarWatch/Frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url'
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -10,6 +11,11 @@ const backendUrl = isDevelopment
 
 const defaultConfig = {
   plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
 }
 
 // https://vitejs.dev/config/
